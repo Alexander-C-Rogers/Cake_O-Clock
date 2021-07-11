be81680d9514b397fbd5d5342f38926c174db6aa
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-11 19:22:36 -0400

    create catch 404 and forward to error handler

--- app.js
+++ app.js
@@ -18,3 +18,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
